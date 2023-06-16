979d2b30de8aeaff6d459dc2044b90be0d4c54e7
Dumitru C <canteadumitru@gmail.com>
2023-06-16 11:06:33 +0300

    Update 20230616105535_voter-fix.ts

--- src/services/db/migrations/20230616105535_voter-fix.ts
+++ src/services/db/migrations/20230616105535_voter-fix.ts
@@ -1,17 +1,43 @@
 import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("PollOptionVote");
+  await knex.schema.dropTable("Voter");
+
   await knex.schema.alterTable("Voter", (table) => {
-    table.string("id", 40).primary().notNullable().alter();
-    table.string("uid", 40).notNullable().unique().alter();
-    table.string("ip", 40).notNullable().alter();
-    table.string("userId", 40).nullable().alter();
+    table.string("id", 40).primary();
+    table.string("uid", 40).notNullable().unique();
+    table.string("ip", 40).notNullable();
+    table.string("userId", 40).nullable();
+
+    table.dateTime("createdAt").notNullable().defaultTo(knex.raw("NOW()"));
+    table.dateTime("updatedAt").notNullable().defaultTo(knex.raw("NOW()"));
   });
 
-  await knex.raw(`DELETE FROM "PollOptionVote"`);
+  await knex.schema.createTable("PollOptionVote", (table) => {
+    table.string("id", 40).notNullable().primary();
+    table
+      .string("voterId", 40)
+      .notNullable()
+      .references("Voter.id")
+      .index()
+      .onDelete("CASCADE");
+    table
+      .string("pollId", 40)
+      .notNullable()
+      .references("Poll.id")
+      .index()
+      .onDelete("CASCADE");
+    table
+      .string("pollOptionId", 40)
+      .notNullable()
+      .references("PollOption.id")
+      .onDelete("CASCADE");
+    table.string("ip", 40).notNullable();
+    table.dateTime("createdAt").notNullable().defaultTo(knex.raw("NOW()"));
+    table.dateTime("updatedAt").notNullable().defaultTo(knex.raw("NOW()"));
 
-  await knex.schema.alterTable("PollOptionVote", (table) => {
-    table.string("voterId", 40).alter();
+    table.unique(["voterId", "pollOptionId"]);
   });
 }
 
